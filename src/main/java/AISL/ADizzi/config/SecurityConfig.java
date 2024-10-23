95fc73137725de7f03e95892e4cbe8e235307a9e
Seungmoky <xkwksdnjs@gmail.com>
2024-10-24 04:03:58 +0900

    /mail - 이메일 발송 성공 , /join - 이미 존재하는 이메일 예외처리

--- src/main/java/AISL/ADizzi/config/SecurityConfig.java
+++ src/main/java/AISL/ADizzi/config/SecurityConfig.java
@@ -13,10 +13,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()  // 요청에 대한 인증 설정
-                .anyRequest().permitAll()  // 모든 요청을 인증 없이 허용
-                .and()
-                .csrf().disable();  // CSRF 보호 비활성화 (필요에 따라 활성화 가능)
+            .authorizeRequests()  // 요청에 대한 인증 설정
+            .anyRequest().permitAll()  // 모든 요청을 인증 없이 허용
+            .and()
+            .csrf().disable();  // CSRF 보호 비활성화 (필요에 따라 활성화 가능)
 
         return http.build();
     }
