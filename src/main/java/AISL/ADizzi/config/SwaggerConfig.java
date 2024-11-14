e76687aee9019bf5fd613119db79e551251706fe
202178038 <202178038@yiu.ac.kr>
2024-11-14 18:00:43 +0900

    woomin test

--- src/main/java/AISL/ADizzi/config/SwaggerConfig.java
+++ src/main/java/AISL/ADizzi/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
         Info info = new Info()
                 .version("v1.0") //버전
                 .title("ADizzi API") //이름
-                .description("AISL ADizzi API"); //설명
+                .description("AISL ADizzi aAPI"); //설명
 
         // SecurityScheme 설정
         SecurityScheme securityScheme = new SecurityScheme()
