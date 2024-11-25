3a83f4c78d8070fd9adda8062763a057aa54953f
202178038 <202178038@yiu.ac.kr>
2024-11-25 16:39:46 +0900

    물건 api 수정 및 물건 이동 api 추가

--- src/main/java/AISL/ADizzi/controller/ItemController.java
+++ src/main/java/AISL/ADizzi/controller/ItemController.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/AISL/ADizzi/service/ItemService.java
+++ src/main/java/AISL/ADizzi/service/ItemService.java
@@ -18,7 +18,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
