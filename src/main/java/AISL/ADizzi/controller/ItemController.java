6e723c8b728fb3dc0dd746650433f810fc210b76
202178038 <202178038@yiu.ac.kr>
2024-11-25 16:29:17 +0900

    물건 api 수정 및 물건 이동 api 추가

--- src/main/java/AISL/ADizzi/controller/ItemController.java
+++ src/main/java/AISL/ADizzi/controller/ItemController.java
@@ -1,6 +1,7 @@
 package AISL.ADizzi.controller;
 
 import AISL.ADizzi.dto.request.CreateItemRequest;
+import AISL.ADizzi.dto.request.MoveItemRequest;
 import AISL.ADizzi.dto.request.UpdateItemRequest;
 import AISL.ADizzi.dto.response.ItemResponse;
 import AISL.ADizzi.service.ItemService;
@@ -19,64 +20,70 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-@Tag(name = "아이템 관련 API")
+@Tag(name = "물건 관련 API")
 public class ItemController {
 
     private final ItemService itemService;
 
-    @Operation(summary = "수납칸에 속한 물건 목록 조회(최신순,오래된순)")
-    @GetMapping("/room/{room_id}/container/{container_id}/slot/{slot_id}")
+    @Operation(summary = "수납칸에 속한 물건 목록 조회(최신순, 오래된순)")
+    @GetMapping("/room/{roomId}/container/{containerId}/slot/{slotId}/item")
     public ResponseEntity<List<ItemResponse>> getMyItems(
             @RequestHeader("Authorization") String token,
-            @PathVariable Long slot_id,
+            @PathVariable Long slotId,
             @RequestParam(value = "sortBy", defaultValue = "recent") String sortBy)
     {
-        Long memberId = JwtUtil.extractAccessToken(token);
-        List<ItemResponse> items = itemService.getMyItems(memberId,slot_id,sortBy);
+        List<ItemResponse> items = itemService.getMyItems(slotId, sortBy);
         return ResponseEntity.ok(items);
     }
 
     @Operation(summary = "수납칸에 물건 생성")
-    @PostMapping("/room/{room_id}/container/{container_id}/slot/{slot_id}/item")
+    @PostMapping("/room/{roomId}/container/{containerId}/slot/{slotId}/item")
     public ResponseEntity<String> createItem(
             @RequestHeader("Authorization") String token,
-            @PathVariable Long slot_id,
+            @PathVariable Long slotId,
             @Valid @RequestBody CreateItemRequest request)
     {
-
         Long memberId = JwtUtil.extractAccessToken(token);
-        itemService.createItem(memberId,slot_id,request);
+        itemService.createItem(memberId, slotId, request);
         return ResponseEntity.ok("success");
     }
 
 
     @Operation(summary ="물건 수정")
-    @PutMapping("/room/{room_id}/container/{container_id}/slot/{slot_id}/item/{item_id}")
+    @PutMapping("/room/{roomId}/container/{containerId}/slot/{slotId}/item/{itemId}")
     public ResponseEntity<String> updateItem(
             @RequestHeader("Authorization") String token,
-            @PathVariable Long slot_id,
-            @PathVariable Long item_id,
+            @PathVariable Long itemId,
             @Valid @RequestBody UpdateItemRequest request)
     {
-
         Long memberId = JwtUtil.extractAccessToken(token);
-        itemService.updateItem(memberId, slot_id, item_id, request);
+        itemService.updateItem(memberId, itemId, request);
+        return ResponseEntity.ok("success");
+    }
+
+    @Operation(summary = "물건 이동")
+    @PatchMapping("/room/{roomId}/container/{containerId}/slot/{slotId}/item/{itemId}")
+    public ResponseEntity<String> moveItem(
+            @RequestHeader("Authorization") String token,
+            @PathVariable Long slotId,
+            @PathVariable Long itemId,
+            @Valid @RequestBody MoveItemRequest request
+    ){
+        Long memberId = JwtUtil.extractAccessToken(token);
+        itemService.moveItem(memberId, slotId, itemId, request);
         return ResponseEntity.ok("success");
     }
 
     @Operation(summary = "물건 삭제")
-    @DeleteMapping("/room/{room_id}/container/{container_id}/slot/{slot_id}/item/{item_id}")
+    @DeleteMapping("/room/{roomId}/container/{containerId}/slot/{slotId}/item/{itemId}")
     public ResponseEntity<String> deleteItem(
             @RequestHeader("Authorization") String token,
-            @PathVariable Long item_id)
+            @PathVariable Long itemId)
     {
         Long memberId = JwtUtil.extractAccessToken(token);
-        itemService.deleteItem(memberId,item_id);
+        itemService.deleteItem(memberId, itemId);
         return ResponseEntity.ok("success");
     }
-
-
-
 
 
 }
